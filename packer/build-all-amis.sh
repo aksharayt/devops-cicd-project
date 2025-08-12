a9d007b29bbea3c6a6dcff6cc6401f4e777da378
aksharayt <aksharaty04@gmail.com>
2025-08-11 22:27:42 -0400

    Update build-all-amis.sh

--- packer/build-all-amis.sh
+++ packer/build-all-amis.sh
@@ -6,49 +6,49 @@ echo "This will take approximately 30-45 minutes total"
 # Make all scripts executable
 # chmod +x scripts/*.sh
 
-# # Build Web Server AMI
-# echo "Building Web Server AMI..."
-# packer build webserver.pkr.hcl
-# if [ $? -ne 0 ]; then
-#     echo "Web Server AMI build failed!"
-#     exit 1
-# fi
-# echo "Web Server AMI build completed!"
+# Build Web Server AMI
+echo "Building Web Server AMI..."
+packer build webserver.pkr.hcl
+if [ $? -ne 0 ]; then
+    echo "Web Server AMI build failed!"
+    exit 1
+fi
+echo "Web Server AMI build completed!"
 
-# # Build Elasticsearch AMI
-# echo "Building Elasticsearch AMI..."
-# packer build elasticsearch.pkr.hcl
-# if [ $? -ne 0 ]; then
-#     echo "Elasticsearch AMI build failed!"
-#     exit 1
-# fi
-# echo "Elasticsearch AMI build completed!"
+# Build Elasticsearch AMI
+echo "Building Elasticsearch AMI..."
+packer build elasticsearch.pkr.hcl
+if [ $? -ne 0 ]; then
+    echo "Elasticsearch AMI build failed!"
+    exit 1
+fi
+echo "Elasticsearch AMI build completed!"
 
-# # Build Kibana AMI
-# echo "Building Kibana AMI..."
-# packer build kibana.pkr.hcl
-# if [ $? -ne 0 ]; then
-#     echo "Kibana AMI build failed!"
-#     exit 1
-# fi
-# echo "Kibana AMI build completed!"
+# Build Kibana AMI
+echo "Building Kibana AMI..."
+packer build kibana.pkr.hcl
+if [ $? -ne 0 ]; then
+    echo "Kibana AMI build failed!"
+    exit 1
+fi
+echo "Kibana AMI build completed!"
 
-# # Build MySQL Primary AMI
-# echo "Building MySQL Primary AMI..."
-# packer build mysql-primary.pkr.hcl
-# if [ $? -ne 0 ]; then
-#     echo "MySQL Primary AMI build failed!"
-#     exit 1
-# fi
-# echo "MySQL Primary AMI build completed!"
+# Build MySQL Primary AMI
+echo "Building MySQL Primary AMI..."
+packer build mysql-primary.pkr.hcl
+if [ $? -ne 0 ]; then
+    echo "MySQL Primary AMI build failed!"
+    exit 1
+fi
+echo "MySQL Primary AMI build completed!"
 
-# # Build MySQL Standby AMI
-# echo "Building MySQL Standby AMI..."
-# packer build mysql-standby.pkr.hcl
-# if [ $? -ne 0 ]; then
-#     echo "MySQL Standby AMI build failed!"
-#     exit 1
-# fi
+# Build MySQL Standby AMI
+echo "Building MySQL Standby AMI..."
+packer build mysql-standby.pkr.hcl
+if [ $? -ne 0 ]; then
+    echo "MySQL Standby AMI build failed!"
+    exit 1
+fi
 echo "MySQL Standby AMI build completed!"
 
 echo "All AMIs built successfully!"
